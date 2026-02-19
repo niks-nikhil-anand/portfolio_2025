97b1b5c4844ccc7f542586bb12e30ebf1dfede2c
niks-nikhil-anand <niks.anand.developer@gmail.com>
2026-02-20 02:33:53 +0530

    Refactor: Categorize skills, adjust hero and experience section layouts, and remove the AboutMe component from the homepage.

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeroSections from '@/components/(Homepage)/HeroSections'
+// import AboutMe from '@/components/(Homepage)/AboutMe'
 import ContactSection from '@/components/(Homepage)/ContactSection'
 import ProjectsShowcase from '@/components/(Homepage)/ProjectsShowcase'
 import SkillsAndContact from '@/components/(Homepage)/SkillsAndContact'
@@ -10,6 +11,7 @@ const page = () => {
   return (
     <div>
       <HeroSections />
+      {/* <AboutMe /> */}
       <Experience />
       <SkillsAndContact />
       <ProjectsShowcase />
